1f8a2e99d5e9bc876932c7bb580705c056d8629f
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-12-08 16:53:46 +0100

    autre changement

--- my-project/config/admin.js
+++ my-project/config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: '4ac50eb7015a2cfd828011c15ee35a5062359eb9abe50f40523cdad3a20afccf731d8dcf841e5d3933e04c7e9fc1f2d39a4ab311b948c3264a5d91a4be0c04d36bb1e6a32419cff71fd3655c78d55ea126fcce663c4d61352f7d46eb0c76cf84c850d2db729719114c1bd23ad1b7d901052e4d320b5530110bcb005e5d5eb42a',
   },
   transfer: {
     token: {
